d43a439a6c3bb0e4d529a03eba548aabef323cbd
zitleu <dizs1337@icloud.com>
2026-05-11 02:03:04 +0500

    added server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, Bookshelf!")
+
+	http.HandleFunc("/health", health)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+
+	response := map[string]string{"status": "ok"}
+
+	json.NewEncoder(w).Encode(response)
 }
