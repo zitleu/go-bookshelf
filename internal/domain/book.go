e6bf122f96b8ceb14fadf1dab0fdb96ff1162c3b
zitleu <dizs1337@icloud.com>
2026-05-16 22:24:53 +0500

    fix

--- internal/domain/book.go
+++ internal/domain/book.go
@@ -25,7 +25,7 @@ type BookResponse struct {
 	ID            uuid.UUID    `json:"id"`
 	Title         string       `json:"title"`
 	Author        string       `json:"author"`
-	CreatedBy     uuid.UUID    `json:"created_by"`
+	CreatedBy     string       `json:"created_by"`
 	CreatedAt     time.Time    `json:"created_at"`
 	UpdatedAt     time.Time    `json:"updated_at"`
 	Description   *string      `json:"description"`
